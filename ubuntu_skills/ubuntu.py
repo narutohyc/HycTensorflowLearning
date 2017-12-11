447ebb17bf245d9e2df66a644d276643428b7aac
narutohyc <1832044043@qq.com>
2017-12-11 10:07:16 +0800

    創建ubuntu_skills文件
    此處存放ubuntu下一些命令使用記錄

---
+++ ubuntu_skills/ubuntu.py
@@ -0,0 +1,35 @@
+1)
+查看本機安裝軟件（管道過濾）： 
+ubuntu@ubuntu:~$ dpkg --list | grep xrd
+ii  xrdp                                       0.6.1-2                                      amd64        Remote Desktop Protocol (RDP) server
+卸載安裝軟件：
+sudo apt-get --purge remove xrdp
+
+2)
+ubuntu好看主題安裝及使用：
+安裝：
+sudo apt-get unity-tweak-tool
+打開：
+unity-tweak-tool
+
+3)
+ubuntu下配置路徑文件：
+~/.bashrc
+source ~/.bashrc  #生效
+或
+/etc/profile
+source /etc/profile  #生效
+
+4）
+ubuntu backup
+/home/hyc/.cache/dconf
+/home/hyc/.dbus
+system setting:
+backup
+
+5)
+
+
+
+
+
