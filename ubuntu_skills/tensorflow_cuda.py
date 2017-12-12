c9f3e31e30bdaab18a7c113db125c31c696a9a2e
narutohyc <1832044043@qq.com>
2017-12-12 14:30:00 +0800

    Update tensorflow_cuda.py

--- ubuntu_skills/tensorflow_cuda.py
+++ ubuntu_skills/tensorflow_cuda.py
@@ -2,5 +2,6 @@ http://city.shaform.com/blog/2016/10/31/install-tensorflow-with-cuda.html
 
 https://www.tensorflow.org/versions/r1.3/install/install_linux
 
-
-
+pycharm add system Environment Variables:
+Preferences > Console > Python Console > Environment Variables, setting
+sys.path.extend('/usr/local/cuda/lib64')
