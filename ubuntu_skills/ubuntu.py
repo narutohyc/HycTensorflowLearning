2ecc47cac68a27c0cafcdc8e097d17527e01b76d
narutohyc <1832044043@qq.com>
2017-12-13 14:15:04 +0800

    Update ubuntu.py

--- ubuntu_skills/ubuntu.py
+++ ubuntu_skills/ubuntu.py
@@ -31,11 +31,15 @@ backup
 问题描述：
 windows 无法安装到这个磁盘。 选中的磁盘具有MBR分区表。 在EFI系统上，Windows只能安装到GPT 磁盘
 
+方案：
+按Shift + F10组合键打开命令提示符窗口（有些笔记本可能需要同时按住Fn键）
+X:>\Sources> diskpart    #输入diskpart命令后回车即可打开Microsoft DiskPart分区工具。
 DISKPART> list disk
-DISKPART> select disk 0
+DISKPART> select disk 0     #输入命令 select disk 0 后回车，这样就选中了第一块硬盘
 DISKPART> clean
-DISKPART> convert gpt
+DISKPART> convert gpt     #输入 convert gpt 命令后回车，即可把当前选中硬盘转换为GPT格式
 DiskPart 已将所选磁盘成功地更换为 GPT 格式
+运行 list partition 命令查看已经创建的分区信息
 
 6）
 
