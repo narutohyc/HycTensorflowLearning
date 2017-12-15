743800bfd742f49bc755b408412ba400b196e79e
narutohyc <1832044043@qq.com>
2017-12-15 10:22:27 +0800

    Update tensorflow_cuda.py

--- ubuntu_skills/tensorflow_cuda.py
+++ ubuntu_skills/tensorflow_cuda.py
@@ -1,7 +1,30 @@
+1）安装tensorflow、cuda、显卡驱动等
 http://city.shaform.com/blog/2016/10/31/install-tensorflow-with-cuda.html
+http://coldmooon.github.io/2015/08/03/caffe_install/
+  
 
+2）tensorflow1.3版本
 https://www.tensorflow.org/versions/r1.3/install/install_linux
 
+3）
+http://darren1231.pixnet.net/blog/post/328443879-deep-learning-model--caffe-%E4%BD%BF%E7%94%A8%E6%95%99%E5%AD%B8
+  
+4）Ubuntu 16.04 安装英伟达（Nvidia）显卡驱动
+https://gist.github.com/dangbiao1991/7825db1d17df9231f4101f034ecd5a2b  
+
+5）pycharm添加环境变量
 pycharm add system Environment Variables:
 Preferences > Console > Python Console > Environment Variables, setting
 sys.path.extend('/usr/local/cuda/lib64')
+
+
+
+6）
+
+
+
+7）
+
+
+
+
