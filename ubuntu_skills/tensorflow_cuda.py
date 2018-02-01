e6b6034d63f84127ae51aaf5e919c201d09b68b6
narutohyc <1832044043@qq.com>
2018-02-01 21:58:18 +0800

    Update tensorflow_cuda.py

--- ubuntu_skills/tensorflow_cuda.py
+++ ubuntu_skills/tensorflow_cuda.py
@@ -1,26 +1,28 @@
 1）安装tensorflow、cuda、显卡驱动等
-http://city.shaform.com/blog/2016/10/31/install-tensorflow-with-cuda.html
-http://coldmooon.github.io/2015/08/03/caffe_install/
+  http://city.shaform.com/blog/2016/10/31/install-tensorflow-with-cuda.html
+  http://coldmooon.github.io/2015/08/03/caffe_install/
   
 
 2）tensorflow1.3版本
-https://www.tensorflow.org/versions/r1.3/install/install_linux
+  https://www.tensorflow.org/versions/r1.3/install/install_linux
+  sudo pip install tensorflow-gpu==1.3.0
 
 3）
-http://darren1231.pixnet.net/blog/post/328443879-deep-learning-model--caffe-%E4%BD%BF%E7%94%A8%E6%95%99%E5%AD%B8
+  http://darren1231.pixnet.net/blog/post/328443879-deep-learning-model--caffe-%E4%BD%BF%E7%94%A8%E6%95%99%E5%AD%B8
   
 4）Ubuntu 16.04 安装英伟达（Nvidia）显卡驱动
-https://gist.github.com/dangbiao1991/7825db1d17df9231f4101f034ecd5a2b  
+  https://gist.github.com/dangbiao1991/7825db1d17df9231f4101f034ecd5a2b  
+  好像可以直接安装cuda
 
 5）pycharm添加环境变量
-pycharm add system Environment Variables:
-Preferences > Console > Python Console > Environment Variables, setting
-sys.path.extend('/usr/local/cuda/lib64')
+  pycharm add system Environment Variables:
+  Preferences > Console > Python Console > Environment Variables, setting
+  sys.path.extend('/usr/local/cuda/lib64')
 
 
 
 6）python install opencv
-pip install opencv-python
+  pip install opencv-python
 
 
 7）tensorflow讀取大數據
