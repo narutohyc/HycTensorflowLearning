44379692425487310e05b144df4f32eb9245acf5
narutohyc <1832044043@qq.com>
2018-03-06 18:24:24 +0800

    创建python学习笔记
    数组按照某关键字比较

---
+++ utils/python_learn.py
@@ -0,0 +1,7 @@
+1. 数组按照某关键字比较
+  >>> pairs = [(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')]
+  >>> pairs.sort(key=lambda pair: pair[1])
+  >>> pairs
+  [(4, 'four'), (1, 'one'), (3, 'three'), (2, 'two')]
+  
+2.
