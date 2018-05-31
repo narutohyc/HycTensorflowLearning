d9c48504f74751e98e2cbd09969fd4456554b90f
narutohyc <1832044043@qq.com>
2018-05-31 11:34:32 +0800

    圖像倍數復制
    圖像倍數復制

--- utils/image_process.py
+++ utils/image_process.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-1) 圖像擴充np.pad
+1) 圖像擴充np.pad  鏡像反轉復制
     import numpy as np
     from skimage import io
     image = io.imread('/home/hyc/code/dehaze/TestVirtualimages/ground_truth_image/art.png')
@@ -11,9 +11,18 @@
     io.imsave('/home/hyc/code/dehaze/art.png',imga)
     # io.imshow(imga)
 
-2)
+2)  圖像倍數復制
+    img = io.imread(join(imgpath, imgList[ii]))
+    padd = 3 #倍數
+    imgdou = np.zeros([img.shape[0]*padd,img.shape[1]*padd,3])
+    for jj in range(padd):
+        for kk in range(padd):
+            imgdou[jj*img.shape[0]:(jj+1)*img.shape[0],kk*img.shape[1]:(kk+1)*img.shape[1],:] = np.uint8(img[:,:,:])
+    imgdou = np.uint8(imgdou)
+    
+3）xxx
 
-
+4) xxx
 
 
 
